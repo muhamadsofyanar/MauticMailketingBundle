db2eef38b0486d8e8c39ffa6f6f00bd8b23edb60
Muhamad Sofyan AR <2259060049@student.uinsgd.ac.id>
2026-08-31 21:38:58 +0700

    Update config.php

--- Config/config.php
+++ Config/config.php
@@ -7,4 +7,29 @@ return [
     'description' => 'Mailketing API transport for Mautic 5',
     'version'     => '5.0.0',
     'author'      => 'Fadli Dzil Ikram',
+    'services'    => [
+        'events' => [
+            'mautic.mailketing.callback_subscriber' => [
+                'class'     => \MauticPlugin\MauticMailketingBundle\EventSubscriber\CallbackSubscriber::class,
+                'arguments' => [
+                    'mautic.email.model.transport_callback',
+                    'mautic.helper.core_parameters',
+                ],
+            ],
+        ],
+        'other' => [
+            'mautic.mailketing.transport_factory' => [
+                'class'     => \MauticPlugin\MauticMailketingBundle\Mailer\Transport\MailketingApiTransportFactory::class,
+                'arguments' => [
+                    'event_dispatcher',
+                    'mautic.mailketing.http_client',
+                    'monolog.logger.mautic',
+                ],
+                'tag' => 'mailer.transport_factory',
+            ],
+            'mautic.mailketing.http_client' => [
+                'class' => \Symfony\Component\HttpClient\NativeHttpClient::class,
+            ],
+        ],
+    ],
 ];
